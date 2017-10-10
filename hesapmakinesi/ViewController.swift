0911184e2d52650ac6f97f4257e7587ff6641154
Yusuf Kutlay <Yusiff@Yusuf-MacBook-Pro.local>
2017-10-10 18:59:32 +0300

    Initial Commit

---
+++ hesapmakinesi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hesapmakinesi
+//
+//  Created by Yusuf Kutlay on 10.10.2017.
+//  Copyright © 2017 Yusuf Kutlay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
