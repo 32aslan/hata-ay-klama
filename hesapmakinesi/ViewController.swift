a786a8260014508df632ce306620ab800bd9f764
32aslan <dexter3291@gmail.com>
2017-10-16 21:49:29 +0300

    Update ViewController.swift

--- hesapmakinesi/ViewController.swift
+++ hesapmakinesi/ViewController.swift
@@ -9,17 +9,96 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum hata: Error{
+        case infHtasi
+        case nanHatasi
+    }
 
+    @IBOutlet weak var sonuc: UILabel!
+    @IBOutlet weak var giris1: UITextField!
+    @IBOutlet weak var giris2: UITextField!
+    
+    @IBAction func toplamaİslemi(_ sender: Any) {
+        if let ilkNumara = Int(giris1.text!){
+            if let ikinciNumara = Int (giris2.text!){
+                let  islemSonucu = ilkNumara+ikinciNumara
+                sonuc.text = String(islemSonucu)
+                
+            }
+        }
+        else {
+            let hata = "Sayı Giriniz..."
+            sonuc.text = String(hata)
+            
+            
+        }
+    }
+    
+    @IBAction func cikarmaİslemi(_ sender: Any) {
+        if let ilkNumara = Int(giris1.text!){
+            if let ikinciNumara = Int (giris2.text!){
+                let  islemSonucu = ilkNumara-ikinciNumara
+                sonuc.text = String(islemSonucu)
+            }
+        }
+    }
+    
+    @IBAction func carpmaİslemi(_ sender: Any) {
+        if let ilkNumara = Int(giris1.text!){
+            if let ikinciNumara = Int (giris2.text!){
+                let  islemSonucu = ilkNumara*ikinciNumara
+                sonuc.text = String(islemSonucu)
+            }
+        }
+    }
+    
+    @IBAction func bölmeİslemi(_ sender: Any)  {
+            let bolunen = Int(giris1.text!)
+            let bolen = Int (giris2.text!)
+
+        func bolme() throws -> Int {
+                    guard bolen != 0 else {
+                        throw hata.nanHatasi
+                    }
+                    guard bolunen != 0 else {
+                        throw hata.infHtasi
+                    }
+            return bolunen!/bolen!
+                    }
+        do {
+            try bolme()
+        } catch hata.nanHatasi {
+            sonuc.text = "Hata"
+        }
+                
+                
+       
+            
+                
+                
+                }
+                
+
+        
+                
+
+        
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sonuc.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+            
 }
+
+
+
 
